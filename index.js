30b1df804dddb2f356b7a0c9dfffd35110662529
Kuldeeep Khutela <khutelakuldeep52@gmail.com>
2024-03-17 17:06:34 +0530

    Final

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173","http://localhost:5174","https://checking-h1aq.vercel.app"]
+   // origin: ["http://localhost:5173","http://localhost:5174","https://checking-h1aq.vercel.app"]
+    origin:"*"
   
   })
 );
